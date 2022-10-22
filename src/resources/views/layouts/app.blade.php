7fa4f5e60b523d8a78600b50e2ca38410c008194
shunyonai1996 <y_hhns_1028@icloud.com>
2022-10-22 17:17:17 +0900

    validation

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -24,8 +24,8 @@
                 </div>
             </nav>
         </header>
-
         <div class="container">
+            @include('commons.error_messages')
             @yield('content')
         </div>
 
--- src/resources/views/tasks/create.blade.php
+++ src/resources/views/tasks/create.blade.php
@@ -2,14 +2,6 @@
 
 @section('content')
 
-  @if (count($errors) > 0)
-    <ul class="alert alert-danger" role='alert'>
-      @foreach ($errors->all() as $error)
-        <li class="ml-4">{{ $error }}</li>
-      @endforeach
-    </ul>
-  @endif
-
 <div class="row">
   <div class="col-6">
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
